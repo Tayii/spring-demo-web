e3a7be63bcffc62ea35df3a5592d16676bf8a17b
Tayii <tai_saito@mail.ru>
2019-09-07 15:28:52 +0500

    posts v2

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -1,4 +1,21 @@
 package com.example.demoweb.service;
 
+import com.example.demoweb.model.Post;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
 public class PostService {
+    private ArrayList<Post> allPosts;
+
+    public ArrayList<Post> listAllPosts() {
+        if (this.allPosts == null) {
+            this.allPosts = new ArrayList<>();
+            this.allPosts.add(new Post("Kappa"));
+            this.allPosts.add(new Post("Keepo"));
+            this.allPosts.add(new Post("PogChamp"));
+        }
+        return this.allPosts;
+    }
 }
